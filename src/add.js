d717f6a12862371d4e4dbe89819a86f5d8304c27
Benedikt Runar <benedikt17@ru.is>
2018-10-13 16:41:48 +0000

    Kóðinn í add hreinskrifaður

--- src/add.js
+++ src/add.js
@@ -1,5 +1,5 @@
 function add(input){
-    //Ef strengurinn er tómur er skilað 0
+    
     if(input === ""){
         return 0;
     }
@@ -7,13 +7,9 @@ function add(input){
     if(input.includes(",")){
         //input skipt upp í hluta sem eru aðgreindir með ","
         var inputArray = input.split(",");
-        //farið í gegnum allan array-inn og summa talnanna fundin
-        var sum = 0;
-        for(var i = 0; i < inputArray.length; i++){
-            sum += parseInt(inputArray[i]);
-        }
-        //sum skilað
-        return sum;
+
+        //summu talnanna skilað
+        return sum(inputArray);
     }
 
     //Ef það er ein tala í input-inu er henni skilað
@@ -21,3 +17,13 @@ function add(input){
 }
 
 module.exports = add;
+
+function sum(inputArray){
+    var sum = 0;
+    
+    for(var i = 0; i < inputArray.length; i++){
+        sum += parseInt(inputArray[i]);
+    }
+    
+    return sum;
+}
