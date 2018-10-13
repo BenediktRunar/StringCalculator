06ca041bef9305334c9d7816b64d182272808318
Benedikt Runar <benedikt17@ru.is>
2018-10-13 20:41:50 +0000

    Bætt við function-inu negativeCheck sem er kallað í í function-inu sum. negativeCheck athugar hvort mínus tala sé í strengnum og kastar villu með öllum mínustölum strengsins ef svo er
    Skrifað test fyrir virkni negativeCheck
    
    Einnig var kóðinn í add function-inu hreinskrifaður til að gera hann skiljanlegri

--- src/add.js
+++ src/add.js
@@ -1,30 +1,51 @@
 function add(input){
-    
     if(input === ""){
         return 0;
     }
+    
+    //input skipt upp í hluta sem eru aðgreindir með annaðhvort "," eða "\n"
+    var inputArray = input.split(/[\,\n]/);
 
-    if(input.includes(",")){
-        //input skipt upp í hluta sem eru aðgreindir með "," eða "\n"
-        var inputArray = input.split(/[\,\n]/);
-        
-
-        //summu talnanna skilað
-        return sum(inputArray);
-    }
-
-    //Ef það er ein tala í input-inu er henni skilað
-    return parseInt(input);
+    //summu talnanna skilað
+    return sum(inputArray);
 }
 
 module.exports = add;
 
 function sum(inputArray){
     var sum = 0;
+    var negativeNumbers;
+    var j = 0;
     
     for(var i = 0; i < inputArray.length; i++){
+        //Kastar villu ef það er neikvæð tala í strengnum
+        negativeCheck(inputArray, i);
+
         sum += parseInt(inputArray[i]);
     }
     
     return sum;
+}
+
+function negativeCheck(inputArray, i){
+    var isNegative = false;
+    var negativeNumbers = "";
+
+    for(; i < inputArray.length; i++){
+        
+        if(parseInt(inputArray[i]) < 0){
+            if(isNegative){
+                negativeNumbers += "," + inputArray[i];
+            }
+            else{
+                negativeNumbers += inputArray[i];
+            }
+            
+            isNegative = true;
+        }
+    }
+    
+    if(isNegative){
+        throw new Error("Negatives not allowed:" + negativeNumbers);
+    }
 }
--- src/add.test.js
+++ src/add.test.js
@@ -19,3 +19,9 @@ test("tests whether a string containing multiple numbers will return their sum o
 test("tests whether a string that divides numbers by \n will return their sum or not", () => {
 	expect(add("1\n2,3")).toBe(6);
 });
+
+test("tests whether a string that containing negative numbers will cause an exception to be thrown", () => {
+    expect(function(){add("2,-4,3,-5");}).toThrow("Negatives not allowed:-4,-5");    
+});
+
+//"Negatives not allowed:-4,-5"
